5e09c35e27ed21f1a086e529b90c34a707d35df4
Emily Roberts <eroberts3523@conestogac.on.ca>
2024-07-26 18:12:17 -0400

    fixed code for validate_wrong_position()

--- CSCN71030 24S - Group 2/validateWrongPosition.c
+++ CSCN71030 24S - Group 2/validateWrongPosition.c
@@ -7,12 +7,21 @@
 int validate_wrong_position(int* guess, int* target, int length) {
 
 	int count = 0;
-	for (int i = 0; i < length; i++) {
+	for (int i = 0; i < length; i++) { // i iterates through guess array
+		int prev_occurances = 0;
+		for (int j = 0; j < i; j++) { // j iterates through guess array before index i
+			if (guess[j] == guess[i]) { // check for previous occcurances of same element
+				prev_occurances++;
+			}
+		}
 		int match_count = 0;
-		for (int j = 0; j < length; j++) {
-			if ((i != j) && (guess[i] == target[j]) && (match_count == 0)) {
-				count++;
+		for (int k = 0; k < length; k++) { // k iterates through target array
+			if ((guess[i] == target[k]) && (i != k) && (guess[k] != target[k])) { // check: values are equal, not at same index
 				match_count++;
+			}
+			if (match_count > prev_occurances) { // check match hasn't been counted for previous occurance of value
+				count++;
+				break;
 			}
 		}
 	}
